18575ca65dbb8fb6077722be63db158e1c0cdbe4
Terrell Lemons <lemonsterrell43@gmail.com>
2020-06-02 14:54:44 -0700

    Updates

---
+++ precache-manifest.22019854f86760e0d525a69516a14c19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91f5518771ac44168cde6852c1fb788d",
+    "url": "/Project-4-Assessment/index.html"
+  },
+  {
+    "revision": "746d534b7cb3e59d5eac",
+    "url": "/Project-4-Assessment/static/css/main.6027c796.chunk.css"
+  },
+  {
+    "revision": "3357667a71712448bd75",
+    "url": "/Project-4-Assessment/static/js/2.2018531e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Project-4-Assessment/static/js/2.2018531e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "746d534b7cb3e59d5eac",
+    "url": "/Project-4-Assessment/static/js/main.7bde6d1a.chunk.js"
+  },
+  {
+    "revision": "4ce392cd79b9ba623764",
+    "url": "/Project-4-Assessment/static/js/runtime-main.18e04ace.js"
+  }
+]);
